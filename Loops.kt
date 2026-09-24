7d685105195cf5d3ea6fd57b7b501e2fa576469c
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2026-09-24 17:08:43 +0700

    Add Loops.kt to demonstrate the use of for and while loops in Kotlin

---
+++ Loops.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // For
+    for (number in 1..5) {
+        // number is the iterator and 1..5 is the range
+        print(number)
+    }
+    println()
+    // 12345
+
+    // While
+    var i = 1
+    while (i <= 5) {
+        print(i)
+        i++
+    }
+    println()
+}
