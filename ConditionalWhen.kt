89920908e2b37f3c297bf9633f34324b8270d203
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2026-09-24 17:04:53 +0700

    Add ConditionalWhen.kt to demonstrate the use of 'when' as a statement and expression in Kotlin

---
+++ ConditionalWhen.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val obj = "Hello"
+
+    // When sebagai statement
+    when (obj) {
+        // Checks whether obj equals to "1"
+        "1" -> println("One")
+        // Checks whether obj equals to "Hello"
+        "Hello" -> println("Greeting")
+        // Default statement
+        else -> println("Unknown")
+    }
+    // Greeting
+
+    // When sebagai expression
+    val result = when (obj) {
+        // If obj equals "1", sets result to "one"
+        "1" -> "One"
+        // If obj equals "Hello", sets result to "Greeting"
+        "Hello" -> "Greeting"
+        // Sets result to "Unknown" if no previous condition is satisfied
+        else -> "Unknown"
+    }
+    println(result)
+    // Greeting
+}
